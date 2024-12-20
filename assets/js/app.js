e43c721e51df11fa8a0d81ba596eb793c5ac2f9c
Andoni Sanchez <andsanace@gmail.com>
2024-12-20 10:55:33 +0100

    First commit

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+let images = [
+  "assets/media/img/carrusel/msg1.jpg",
+  "assets/media/img/carrusel/rebuild.png",
+  "assets/media/img/carrusel/cuartel.jpg",
+  "assets/media/img/carrusel/msg2.png",
+];
+let carrusel = document.querySelector("#carrusel");
+let img = document.createElement("img");
+img.setAttribute("src", images[0]);
+carrusel.appendChild(img);
+let i = 1;
+window.setInterval(() => {
+  if (i > images.length - 1) i = 0;
+  img.setAttribute("src", images[i]);
+  console.log(i);
+  i++;
+}, 5000);
