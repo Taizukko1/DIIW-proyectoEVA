e6b58b8e382fc7e718ddc6eb5851909db21fbe7f
Andoni Sanchez <andsanace@gmail.com>
2025-01-15 14:15:07 +0100

    v3

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,46 +1,51 @@
-let accesibiladStatus =
-  sessionStorage.getItem("theme") != null ? sessionStorage.getItem("theme") : 0;
-let images = [
-  "assets/media/img/carrusel/msg1.jpg",
-  "assets/media/img/carrusel/rebuild.png",
-  "assets/media/img/carrusel/cuartel.jpg",
-  "assets/media/img/carrusel/msg2.png",
-];
-let carrusel = document.querySelector("#carrusel");
-if(carrusel != null) {
-  let img = document.createElement("img");
-  img.setAttribute("src", images[0]);
-  img.setAttribute("alt", `Carrousel Image 0`);
-  carrusel.appendChild(img);
-  let i = 1;
-  window.setInterval(() => {
-    if (i > images.length - 1) i = 0;
-    img.setAttribute("src", images[i]);
-    img.setAttribute("alt", `Carrousel Image ${i}`);
-    console.log(i);
-    i++;
-  }, 5000);
-}
-
-function activarAccesibilidad() {
+window.onload = () => {
+  let accesibiladStatus =
+  localStorage.getItem("theme") != null ? localStorage.getItem("theme") : 0;
   const body = document.body;
-  if (accesibiladStatus === 0) {
+  const toggle = document.querySelector("#toggle");
+  
+  console.log({localStorage: localStorage.getItem("theme"), valor: accesibiladStatus});
+  
+  if (accesibiladStatus == 1) {
     body.classList.add("contrast");
-    accesibiladStatus = 1;
-    sessionStorage.setItem("theme", 1);
+    toggle.innerHTML = "Modo Oscuro";
   } else {
-    body.classList.remove("contrast");
-    accesibiladStatus = 0;
-    sessionStorage.setItem("theme", 0);
+    toggle.innerHTML = "Modo Claro";
   }
-}
-
-function cargarTema() {
-  const body = document.body;
-  if (accesibiladStatus === 1) {
-    alert(1);
-    body.classList.add("contrast");
+  
+  function toggleTheme() {
+    console.log("Cambiando " + accesibiladStatus);
+    if (accesibiladStatus == 0) {
+      body.classList.add("contrast");
+      accesibiladStatus = 1;
+      localStorage.setItem("theme", 1);
+    } else {
+      body.classList.remove("contrast");
+      accesibiladStatus = 0;
+      localStorage.setItem("theme", 0);
+    }
   }
-}
+  
+  toggle.onclick = toggleTheme;
 
-cargarTema();
+  let images = [
+    "assets/media/img/carrusel/msg1.jpg",
+    "assets/media/img/carrusel/rebuild.png",
+    "assets/media/img/carrusel/cuartel.jpg",
+    "assets/media/img/carrusel/msg2.png",
+  ];
+  let carrusel = document.querySelector("#carrusel");
+  if (carrusel != null) {
+    let img = document.createElement("img");
+    img.setAttribute("src", images[0]);
+    img.setAttribute("alt", `Carrousel Image 0`);
+    carrusel.appendChild(img);
+    let i = 1;
+    window.setInterval(() => {
+      if (i > images.length - 1) i = 0;
+      img.setAttribute("src", images[i]);
+      img.setAttribute("alt", `Carrousel Image ${i}`);
+      i++;
+    }, 5000);
+  }
+};
